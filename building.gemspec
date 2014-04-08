267381ae5b12c63fcb73fdfaa3fcfb83419e57e5
Lucas Carlson <lucas@rufy.com>
2014-04-07 23:57:43 -0700

    Adding checks and verbosity to the building process

--- building.gemspec
+++ building.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name        = 'building'
-  s.version     = '1.0.0'
+  s.version     = '1.1.0'
   s.date        = '2014-03-31'
   s.summary     = "Build a Docker container for any app using Heroku Buildpacks"
   s.description = "Build a Docker container for any app using Heroku Buildpacks"
   s.authors     = ["Lucas Carlson"]
   s.email       = 'lucas@rufy.com'
   s.files       = ["lib/building.rb","bin/building"]
-  s.requirements = ['bundler']
+  s.requirements = ['bundler', 'highline']
   s.homepage    = 'http://github.com/CenturyLinkLabs/building'
   s.license     = 'MIT'
   s.executables = ['building']
--- lib/building.rb
+++ lib/building.rb
@@ -1,9 +1,11 @@
 require 'yaml'
 require 'fileutils'
+require 'bundler/setup' 
+require 'highline/import'
 
-class App2Container
+class Building
   def self.convert(app_name, tag, options={})
-    App2Container.new(app_name, tag, options)
+    Building.new(app_name, tag, options)
   end
 
   def initialize(app_name, tag, options={})
@@ -19,57 +21,71 @@ class App2Container
     create_dockerfile
     build_container
 
+    build_fig if @fig
+
     if @port
       run_container(@port)
     else
       explain_container(8080)
     end
 
-    if @fig
-      build_fig
-    end
-
     exit 0
   end
 
   def create_dockerfile
-    File.open("Dockerfile" , "w") do |file|
-      file << "FROM #{@from || "ctlc/buildstep:ubuntu13.10"}\n"
-    end
+    dockerfile = []
+    dockerfile << "FROM #{@from || "ctlc/buildstep:ubuntu13.10"}\n"
 
     if @buildpack_url
-      File.open("Dockerfile" , "a") do |file|
-        file << <<-eof
+      dockerfile << <<-eof
 RUN git clone --depth 1 #{@buildpack_url} /build/buildpacks/#{@buildpack_url.split("/").last.sub(".git","")}
 RUN echo #{@buildpack_url} >> /build/buildpacks.txt
 eof
-      end
     end
 
     if @includes
-      File.open("Dockerfile" , "a") do |file|
-        file << "RUN #{@includes}\n"
-      end
+      dockerfile << "RUN #{@includes}\n"
     end
 
     if @file
-      File.open("Dockerfile" , "a") do |file|
-        file << IO.read(@file)
-      end
+      dockerfile << IO.read(@file)
     end
 
-    File.open("Dockerfile" , "a") do |file|
-      file << <<-eof
+    dockerfile << <<-eof
 ADD . /app
 RUN /build/builder
 CMD /start web
 eof
+    
+    skip = false
+
+    if File.exists?("Dockerfile")
+      if IO.read("Dockerfile") == dockerfile.join("\n")
+        skip = true
+        say %{   <%= color('identical', [BLUE, BOLD]) %>  Dockerfile}
+      else
+        say %{    <%= color('conflict', [RED, BOLD]) %>  Dockerfile}
+        choice = ask("Overwrite Dockerfile? [Yn] ")
+        if choice.downcase == "n"
+          say("Aborting...")
+          exit 1
+        else
+          say %{       <%= color('force', [YELLOW, BOLD]) %>  Dockerfile}
+        end
+      end
+    else
+      say %{      <%= color('create', [GREEN, BOLD]) %>  Dockerfile}
+    end
+
+    if !skip
+      File.open("Dockerfile" , "w") do |file|
+        file << dockerfile.join("\n")
+      end
     end
   end
 
   def build_fig
-    File.open(@fig , "w") do |file|
-      file << <<-eof
+    figfile = <<-eof
 web:
   image: #{@app_name}:#{@tag}
   command: /start web
@@ -78,22 +94,52 @@ web:
   ports:
    - #{@port || 8080}
 eof
+
+    skip = false
+    
+    if File.exists?(@fig)
+      if IO.read(@fig) == figfile
+        skip = true
+        say %{   <%= color('identical', [BLUE, BOLD]) %>  #{@fig}}
+      else
+        say %{    <%= color('conflict', [RED, BOLD]) %>  #{@fig}}
+        choice = ask("Overwrite #{File.expand_path(@fig)}? [Yn] ")
+        if choice.downcase == "n"
+          say("Aborting...")
+          exit 1
+        else
+          say %{       <%= color('force', [YELLOW, BOLD]) %>  #{@fig}}
+        end
+      end
+    else
+      say %{      <%= color('create', [GREEN, BOLD]) %>  #{@fig}}
+    end
+
+    if !skip
+      File.open(@fig , "w") do |file|
+        file << figfile
+      end
     end
   end
 
   def build_container
-    pid = fork { exec "docker build -t #{@app_name}:#{@tag} ." }
+    build_cmd = "docker build -t #{@app_name}:#{@tag} ."
+    say %{    <%= color('building', [BLUE, BOLD]) %>  #{build_cmd}}
+    pid = fork { exec build_cmd }
     Process.waitpid(pid)
   end
 
   def explain_container(port)
     run = "docker run -d -p #{port} -e \"PORT=#{port}\" #{@app_name}:#{@tag}"
-    puts "\nTo run your app, try something like this:\n\n\t#{run}\n\n"
+    rebuild = "docker build -t #{@app_name} ."
+
+    say "        <%= color('hint', [YELLOW, BOLD]) %>       To run your app, try:  <%= color('#{run}', [BOLD]) %>"
+    say "        <%= color('hint', [YELLOW, BOLD]) %>  To re-build your app, try:  <%= color('#{rebuild}', [BOLD]) %>"
   end
 
   def run_container(port)
-    run = "\ndocker run -d -p #{port} -e \"PORT=#{port}\" #{@app_name}:#{@tag}"
-    puts "#{run}"
+    run = "docker run -d -p #{port} -e \"PORT=#{port}\" #{@app_name}:#{@tag}"
+    say "     <%= color('running', [BLUE, BOLD]) %>  #{run}"
     exec run
   end
 end
